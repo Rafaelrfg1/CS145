14cc6289ac4efcd718e8f8d8412f81befd2c3b5a
raphaelexander <benalexander2020@gmail.com>
2025-01-12 15:39:50 -0800

    Week 1 Project

---
+++ DialUp.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class DialUp {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char num = input.nextChar();
+        
+    }
+    int getNumber(char num){
+        
+    }
+}
